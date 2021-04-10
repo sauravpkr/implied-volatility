b866b397fe1cd8bce9e6bc189973d74b5266ad56
Saurav <sauravtiwari770@gmail.com>
2021-04-10 17:03:07 +0530

    environment varible updated

--- database_operation.py
+++ database_operation.py
@@ -2,10 +2,13 @@ import pymongo
 import urllib
 from pymongo import MongoClient
 from market_data import get_stock_data
-from iv import pull_iv
+#from iv import pull_iv
+import os
 
 def connect_with_database():
-    mongodb_uri= "mongodb+srv://saurav:"+ urllib.parse.quote("Saurav770@")+"@cluster0.z5nma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongodb_user_name=os.environ.get('MONGODB_USER_NAME')
+    mongodb_password= urllib.parse.quote(os.environ.get('MONGODB_PASS'))
+    mongodb_uri= "mongodb+srv://"+mongodb_user_name+":"+ mongodb_password+"@cluster0.z5nma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     client= MongoClient(mongodb_uri)
     global db
     db = client["market_data"]
@@ -26,6 +29,6 @@ if __name__ == "__main__":
     #pull_iv('NIFTY')
     connect_with_database()
     #d1=get_stock_data('NIFTY')
-    data,ce,pe=pull_iv('NIFTY')
-    insert_data_into_database(ce)
+    #data,ce,pe=pull_iv('NIFTY')
+    #insert_data_into_database(ce)
     
